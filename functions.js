45d6a023ab0b3da016322a9fb55066c568f7862a
Stepan345 <79114505+Stepan345@users.noreply.github.com>
2024-12-05 18:43:36 -0500

    Added random AI moves and and evaluation function

--- functions.js
+++ functions.js
@@ -10,24 +10,33 @@ class Game{
      * @param {int} y1 
      * @param {int} x2 
      * @param {int} y2 
-     * @returns {boolean} Returns if the game has ended
+     * @param {boolean} shout Should the function post to console or not
+     * @returns {int} Returns if the game has ended, or if the move was illegal 
+     * Key:
+     * 1 = Game Ended
+     * 0 = Game Didn't end
+     * -1 = Illegal Move
      */
-    makeMove(x1,y1,x2,y2){
-        if(this.gameBoard.board[y1][x1][y2][x2] != null || this.gameBoard.winningBoxes[y1][x1] != 0 || (this.nextBox[0] != -1 && x1 != this.nextBox[0] && y1 != this.nextBox[1] )){
-            console.log("Illegal Move. Reason(s):")
-            if(this.gameBoard.board[y1][x1][y2][x2] != null)console.log("-Not a free box")
-            if(this.gameBoard.winningBoxes[y1][x1] != 0)console.log("-Box already won")
+
+    makeMove(x1,y1,x2,y2,shout = true){
+        if(this.gameBoard.board[y1][x1][y2][x2] != undefined || this.gameBoard.winningBoxes[y1][x1] != 0 || (this.nextBox[0] != -1 && (x1 != this.nextBox[0] || y1 != this.nextBox[1] ))){
+            if(shout)console.log("Illegal Move. Reason(s):")
+            if(this.gameBoard.board[y1][x1][y2][x2] != null && shout)console.log("-Not a free space")
+            if(this.gameBoard.winningBoxes[y1][x1] != 0 && shout)console.log("-Box already won")
+            if((this.nextBox[0] != -1 && x1 != this.nextBox[0] && y1 != this.nextBox[1]) && shout)console.log("-Play made in the wrong box")
+            return -1
         }else{
             this.gameBoard.setBoard(x1,y1,x2,y2,this.turn)
             this.turn = this.turn == "X"? "O": "X"
-            this.nextBox = [x2,y2]
-            if(this.gameBoard.winningBoxes[this.nextBox[0]][this.nextBox[0]] != 0)this.nextBox = [-1,-1]
+            
         }
         this.gameBoard.drawBoard()
+        this.nextBox = [x2,y2]
+        if(this.gameBoard.winningBoxes[this.nextBox[1]][this.nextBox[0]] != 0)this.nextBox = [-1,-1]
         if(this.gameBoard.checkWinner() != 0){
-            return true
+            return 1
         }
-        return false
+        return 0
     }
 }
 class Board{
@@ -60,17 +69,17 @@ class Board{
                     for(let x1 = 0; x1 < 3;x1++){
                         line += "| "
                         for(let x2 = 0;x2 < 3;x2++){
-                            // if(this.winningBoxes[y1][x1] != 0){
-                            //     switch (this.winningBoxes[y1][x1]) {
-                            //         case 1:
-                            //             line += X
-                            //             break;
-                            //         case -1:
-                            //             line += O
-                            //             break;
-                            //     }
-                            // }
-                            /*else*/ if(field[y1][x1][y2][x2] == 1){
+                            if(this.winningBoxes[y1][x1] == -1 || this.winningBoxes[y1][x1] == 1){
+                                switch (this.winningBoxes[y1][x1]) {
+                                    case 1:
+                                        line += X
+                                        break;
+                                    case -1:
+                                        line += O
+                                        break;
+                                }
+                            }
+                            else if(field[y1][x1][y2][x2] == 1){
                                 line += X
                             }else if(field[y1][x1][y2][x2] == -1){
                                 line += O
@@ -117,28 +126,38 @@ class Board{
      * 
      * @param {int} x 
      * @param {int} y 
-     * @returns {int} Who is winning that board {-1: O, 0: No one, 1: X}
+     * @returns {int} Who is winning that board {-1: O, 0: No One, 1: X, 2: Draw}
      */
     checkWinner(x = -1,y = -1){
         this.#updateWinningBoxes()
         if(x == -1){
             for(let j = -1 ; j <= 1; j+=2){
-                if(this.winningBoxes[0][0] == j && this.winningBoxes[0][1] == j && this.winningBoxes[0][2] == j)return j
-                else if(this.winningBoxes[1][0]  == j && this.winningBoxes[1][1] == j && this.winningBoxes[1][2] == j)return j
-                else if(this.winningBoxes[2][0]  == j && this.winningBoxes[2][1] == j && this.winningBoxes[2][2] == j)return j
-                else if(this.winningBoxes[0][0]  == j && this.winningBoxes[1][0] == j && this.winningBoxes[2][0] == j)return j
-                else if(this.winningBoxes[0][1]  == j && this.winningBoxes[1][1] == j && this.winningBoxes[2][1] == j)return j
-                else if(this.winningBoxes[0][2]  == j && this.winningBoxes[1][2] == j && this.winningBoxes[2][2] == j)return j
-                else if(this.winningBoxes[0][0]  == j && this.winningBoxes[1][1] == j && this.winningBoxes[2][2] == j)return j
-                else if(this.winningBoxes[2][0]  == j && this.winningBoxes[1][1] == j && this.winningBoxes[0][2] == j)return j
+                this.testForLineOnBoard(this.winningBoxes,j)
             }
+            let draw = true
+            this.winningBoxes.forEach(element => {
+                if(!draw)return
+                if(element.includes(0))draw = false
+            });
+            if(draw)return 2
             return 0
         }else{
             return this.winningBoxes[y][x]
         }
         
     }
-
+    testForLineOnBoard(board,j){
+        let total = 0
+        if(board[0][0] == j && board[0][1] == j && board[0][2] == j)total ++
+        if(board[1][0]  == j && board[1][1] == j && board[1][2] == j)total ++
+        if(board[2][0]  == j && board[2][1] == j && board[2][2] == j)total ++
+        if(board[0][0]  == j && board[1][0] == j && board[2][0] == j)total ++
+        if(board[0][1]  == j && board[1][1] == j && board[2][1] == j)total ++
+        if(board[0][2]  == j && board[1][2] == j && board[2][2] == j)total ++
+        if(board[0][0]  == j && board[1][1] == j && board[2][2] == j)total ++
+        if(board[2][0]  == j && board[1][1] == j && board[0][2] == j)total ++
+        return total
+    }
     #updateWinningBoxes(){
         for(let i = 0;i < 9; i++){
             let x = i%3
@@ -149,31 +168,7 @@ class Board{
             //Check for Winner
             let targetBoard = this.board[y][x]
             for(let j = -1 ; j <= 1; j+=2){
-                if(targetBoard[0][0] == j && targetBoard[0][1] == j && targetBoard[0][2] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }else if(targetBoard[1][0]  == j && targetBoard[1][1] == j && targetBoard[1][2] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }else if(targetBoard[2][0]  == j && targetBoard[2][1] == j && targetBoard[2][2] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }else if(targetBoard[0][0]  == j && targetBoard[1][0] == j && targetBoard[2][0] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }else if(targetBoard[0][1]  == j && targetBoard[1][1] == j && targetBoard[2][1] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }else if(targetBoard[0][2]  == j && targetBoard[1][2] == j && targetBoard[2][2] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }else if(targetBoard[0][0]  == j && targetBoard[1][1] == j && targetBoard[2][2] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }else if(targetBoard[2][0]  == j && targetBoard[1][1] == j && targetBoard[0][2] == j){
-                    this.winningBoxes[y][x] = j
-                    continue
-                }
+                if(this.testForLineOnBoard(targetBoard,j))this.winningBoxes[y][x] = j
             }
             //Check for Stalemate
             let notFull = false;
@@ -185,5 +180,114 @@ class Board{
         }
     }
 }
+class Computer{
+    constructor(game) {
+        this.game = game
+        this.boardObject = game.gameBoard
+        this.gameBoard = game.gameBoard.board
+    }
+    /**
+     * 
+     * @returns {float}The evaluation of the board state
+     */
+    evaluate(board = this.gameBoard,winningBoxes = this.boardObject.winningBoxes){
+        const weights = [
+            [20,10,20],
+            [10,40,10],
+            [20,10,20],
+        ]
+        const oneAway = 50;
+        const winningBox = 300
+        const oneAwayWinning = 500
+        const WIN = 100000
+        let evaluation = 0
+        let count = 0 
+        board.forEach((ROW,y1) => {
+            ROW.forEach((inner,x1) => {
+                if(winningBoxes[y1][x1] != 0){
+                    if(winningBoxes[y1][x1] != 2){
+                        evaluation += winningBox*winningBoxes[y1][x1]
+                        console.log("Winning Box: ",winningBox)
+                    }
+                    return
+                }
+                inner.forEach((row,y) =>{
+                    for(let x = 0;x<3;x++){
+                        let square = row[x]
+                        //Piece count eval
+                        evaluation += weights[y][x]*(square == undefined?0:square)
+                        if(square != undefined){
+                            count++
+                        }else{
+                            //One away eval
+                            let hyp = [Array.from(inner[0]),Array.from(inner[1]),Array.from(inner[2])]
+                            for(let i = -1 ; i <= 1; i+=2){
+                                hyp[y][x] = i
+                                let numberOfLines = this.boardObject.testForLineOnBoard(hyp,i)
+                                evaluation += numberOfLines*i*oneAway
+                                if(numberOfLines > 0)console.log("One Away: ",numberOfLines*i*oneAway)
+                            }
+                        }
+                        
+
+                    }
+                })
+            })
+        })
+        winningBoxes.forEach((row,y) =>{
+            for(let x = 0;x<3;x++){
+                let square = row[x]
+                
+                if(square == undefined){
+                    let hyp = [Array.from(inner[0]),Array.from(inner[1]),Array.from(inner[2])]
+                    for(let i = -1 ; i <= 1; i+=2){
+                        hyp[y][x] = i
+                        let numberOfLines = this.boardObject.testForLineOnBoard(hyp,i)
+                        evaluation += numberOfLines*i*oneAwayWinning
+                        if(numberOfLines > 0)console.log("One Away From Winning: ",numberOfLines*i*oneAwayWinning)
+                    }
+                }
+                
+
+            }
+        })
+        for(let i = -1 ; i <= 1; i+=2){
+            let numberOfLines = this.boardObject.testForLineOnBoard(winningBoxes,i)
+            evaluation += numberOfLines*i*WIN
+            if(numberOfLines > 0)console.log("Winner: ",numberOfLines*i*WIN)
+        }
+        return evaluation/count
+    }
+    takeTurn(){
+        let x1
+        let y1
+        let x2
+        let y2
+        console.log(this.evaluate())
+        if(this.game.nextBox[0] != -1){
+            x1 = this.game.nextBox[0]
+            y1 = this.game.nextBox[1]
+        }else{
+            let i = false
+            while(!i){
+                x1 = Math.floor(Math.random() * 3)
+                y1 = Math.floor(Math.random() * 3)
+                if(this.boardObject.winningBoxes[y1][x1] == 0)i = true
+            }
+            //console.log(x1,y1)
+        }
+        let i = false
+        while(!i){
+            x2 = Math.floor(Math.random() * 3)
+            y2 = Math.floor(Math.random() * 3)
+            if(this.gameBoard[y1][x1][y2][x2] == undefined)i = true
+        }
+        console.log(x1,y1,x2,y2)
+        let gameState = this.game.makeMove(x1,y1,x2,y2,true)
+        return gameState
+        
+    }
+}
+exports.Computer = Computer
 exports.Board = Board
 exports.Game = Game
--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 var globalBoard = new fn.Board(true)
 var game = new fn.Game(globalBoard)
+var computer = new fn.Computer(game)
 globalBoard.drawBoard(globalBoard)
 
-startTwoPlayerTest()
+
+//startTwoPlayerTest()
+//startOnePlayerTest()
+startNoPlayerTest()
 function startTwoPlayerTest(){
     let gameEnd = false
     while(!gameEnd){
@@ -16,18 +20,55 @@ function startTwoPlayerTest(){
             continue
         }
         let move = answer.split("")
-        let i = 0
-        move.forEach(element => {
-            move[i] = parseInt(element)-1
-            if(move[i] > 2)move[5] = -1
-            i++
-        });
-        if(move.length != 4){
-            console.log("Your move should be 4 chars long and consist of only numbers 3, 2, and 1")
-            continue
-        }
-        if(game.makeMove(move[0],move[1],move[2],move[3])){
+        if(checkMove(move) && game.makeMove(move[0],move[1],move[2],move[3]) == 1){
             console.log("Game Ended By Victory")
+            gameEnd = true
         }
     }
+}
+function startOnePlayerTest(){
+    console.log("You are playing X")
+    let gameEnd = false
+    while(!gameEnd){
+        let answer
+        if(game.turn == "X"){
+            answer = prompt("Make a move as "+game.turn+" > ")
+            if(answer == 'end'){
+                console.log("Force End")
+                gameEnd = true
+                continue
+            }
+            
+            let move = answer.split("")
+            if(game.nextBox[0] != -1) move.unshift(game.nextBox[0]+1,game.nextBox[1]+1)
+            if(checkMove(move) && game.makeMove(move[0],move[1],move[2],move[3]) == 1){
+                console.log("Game Ended By Victory")
+                gameEnd = true
+            }
+        }else{
+            computer.takeTurn()
+        }
+
+    }
+}
+function startNoPlayerTest(){
+    let gameEnd = false
+    while(!gameEnd){
+        let gameState = computer.takeTurn()
+        if(gameState == 1||gameState == -1)gameEnd = true
+    }
+}
+function checkMove(move){
+    let i = 0
+    move.forEach(element => {
+        move[i] = parseInt(element)-1
+        if(move[i] > 2)move[5] = -1
+        i++
+    });
+    if(move.length != 4){
+        console.log("Your move should be 4 chars long and consist of only numbers 3, 2, and 1")
+        return false
+    }
+    else return true
+
 }
